e4ac45d55f4a345bf596f4e84b6e31abb195e40b
Vircon32 <34624915+vircon32@users.noreply.github.com>
2023-01-02 00:39:44 +0100

    Remove GLU/GLUT dependency

--- DesktopEmulator/DesktopInfrastructure/OpenGL2DContext.cpp
+++ DesktopEmulator/DesktopInfrastructure/OpenGL2DContext.cpp
@@ -25,10 +25,35 @@ void ClearOpenGLErrors()
 
 // -----------------------------------------------------------------------------
 
+string GLErrorString( GLenum ErrorCode )
+{
+    switch( ErrorCode )
+    {
+        // opengl 2 errors (8)
+        case GL_NO_ERROR:           return "GL_NO_ERROR";
+        case GL_INVALID_ENUM:       return "GL_INVALID_ENUM";
+        case GL_INVALID_VALUE:      return "GL_INVALID_VALUE";
+        case GL_INVALID_OPERATION:  return "GL_INVALID_OPERATION";
+        case GL_STACK_OVERFLOW:     return "GL_STACK_OVERFLOW";
+        case GL_STACK_UNDERFLOW:    return "GL_STACK_UNDERFLOW";
+        case GL_OUT_OF_MEMORY:      return "GL_OUT_OF_MEMORY";
+        case GL_TABLE_TOO_LARGE:    return "GL_TABLE_TOO_LARGE";
+        
+        // opengl 3 errors (1)
+        case GL_INVALID_FRAMEBUFFER_OPERATION:
+            return "GL_INVALID_FRAMEBUFFER_OPERATION";
+        
+        default:
+            return "Unknown error";
+  }
+}
+
+// -----------------------------------------------------------------------------
+
 void LogOpenGLResult( const string& EntryText )
 {
     GLenum Result = glGetError();
-    string ResultString = (const char * )gluErrorString( Result );
+    string ResultString = GLErrorString( Result );
     
     LOG( EntryText + ": " + ResultString );
     
