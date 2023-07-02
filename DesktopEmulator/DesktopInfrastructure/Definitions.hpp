f6b9067b0cf1a82d3ea75c1a066643929151b678
Vircon32 <34624915+vircon32@users.noreply.github.com>
2023-07-02 16:35:05 +0200

    Remove unused numeric functions in Definitions

--- DesktopEmulator/DesktopInfrastructure/Definitions.hpp
+++ DesktopEmulator/DesktopInfrastructure/Definitions.hpp
@@ -10,15 +10,14 @@
 // =============================================================================
 
 
-bool IsPowerOf2( unsigned Value );
-unsigned NextPowerOf2( unsigned Value );
-
 template<typename T>
 void Clamp( T& Variable, int Mininum, int Maximum )
 {
     if( Variable > Maximum ) Variable = Maximum;
     if( Variable < Mininum ) Variable = Mininum;
 }
+
+// -----------------------------------------------------------------------------
 
 template<typename T>
 void Clamp( T& Variable, double Mininum, double Maximum )
@@ -27,23 +26,24 @@ void Clamp( T& Variable, double Mininum, double Maximum )
     if( Variable < Mininum ) Variable = Mininum;
 }
 
+// -----------------------------------------------------------------------------
+
 inline bool IsBetween( double Value, double Minimum, double Maximum )
 {
     return (Value >= Minimum) && (Value <= Maximum);
 }
 
-// returns -1, 0 or 1
-template<typename T>
-int Sign( T Number )
-{
-    return (Number > 0) - (Number < 0);
-}
+// -----------------------------------------------------------------------------
 
-// define these because STL templates only work with same type arguments
-float Min( float x, int y );
-float Min( int x, float y );
-float Max( float x, int y );
-float Max( int x, float y );
+inline unsigned NextPowerOf2( unsigned Value )
+{
+    unsigned PowerOf2 = 1;
+    
+    while( PowerOf2 < Value )
+      PowerOf2 <<= 1;
+    
+    return PowerOf2;
+}
 
 
 // *****************************************************************************
